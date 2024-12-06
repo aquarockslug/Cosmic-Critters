f8b0690ac80aec9216ab449646f73649be49be5b
aqua <aqua.rock.slug@gmail.com>
2024-12-06 05:13:02 -0600

    highlighted tiles

--- game.js
+++ game.js
@@ -1,85 +1,83 @@
-// black hole puzzle
+// animal scan by Aquarock
 
 setCanvasPixelated(false);
 const cameraOffset = vec2(0, -0.5);
 const backgroundColor = hsl(0, 0, 0.2);
 
-// biome-ignore format:
-let level, levelSize, levelFall, fallTimer, dragStartPos, comboCount,
-	score, bestScore, sound_goodMove, sound_badMove, sound_fall;
+let levelSize;
+const green = rgb(0, 1, 0);
 
-// biome-ignore format:
-{
-sound_goodMove = new Sound([.4,.2,250,.04,,.04,,,1,,,,,3]);
-sound_badMove = new Sound([,,700,,,.07,,,,3.7,,,,3,,,.1]);
-sound_fall = new Sound([.2,,1900,,,.01,,1.4,,91,,,,,,,,,,.7]);
-}
+const tileHighlighted = (pos) => level()[pos.x + pos.y * levelSize.x];
 
-// tiles
-const tileColors = [
-	rgb(1, 0, 0),
-	rgb(1, 1, 1),
-	rgb(1, 1, 0),
-	rgb(0, 1, 0),
-	rgb(0, 0.6, 1),
-	rgb(0.6, 0, 1),
-	rgb(0.5, 0.5, 0.5),
-];
-const tileTypeCount = tileColors.length;
+const level = () => {
+	// a level where none of the tiles are highlighted
+	const emptyLevel = (newLevel = []) => {
+		for (x = levelSize.x; x--; )
+			for (y = levelSize.y; y--; ) newLevel[x + y * levelSize.x] = false;
+		return newLevel;
+	};
+	if (mouseOffLevel()) return emptyLevel(); // mouse not hovering over level area
+	l = emptyLevel();
+	l[mouse().x + mouse().y * levelSize.x] = true;
+	return l;
+};
 
-const getTile = (pos) => level[pos.x + pos.y * levelSize.x];
-const setTile = (pos, data) => {
-	level[pos.x + pos.y * levelSize.x] = data;
+const mouse = () => vec2(Math.floor(mousePos.x), Math.floor(mousePos.y));
+const mouseOffLevel = () => {
+	return (
+		mousePos.x < 0 ||
+		mousePos.y < 0 ||
+		mousePos.x > levelSize.x ||
+		mousePos.y > levelSize.y
+	);
 };
 
 function gameInit() {
-	// setup canvas
-	canvasFixedSize = vec2(1920, 1080);
+	canvasFixedSize = vec2(720, 720);
 	mainCanvas.style.background = backgroundColor;
 
-	// randomize level
-	level = [];
 	levelSize = vec2(5, 5);
-	const pos = vec2();
-	for (pos.x = levelSize.x; pos.x--; )
-		for (pos.y = levelSize.y; pos.y--; )
-			setTile(pos, randInt(tileColors.length));
 
-	// setup game
+	// init animal species list and images
+	// TODO: create animal object
+	animalSpecies = [];
+	animalPositions = [vec2(1, 1), vec2(2, 2)];
+
 	cameraPos = levelSize.scale(0.5).add(cameraOffset);
 	cameraScale = 500 / levelSize.y;
 }
-function gameUpdate() {}
+function gameUpdate() {
+	// randomly spawn animal
+
+	if (mouseWasPressed(0)) {
+		// TODO: use filter function
+		for (animalPos of animalPositions) {
+			if (tileHighlighted(animalPos)) console.log("hit");
+		}
+	}
+
+	// if mouse clicked:
+	// getActiveAnimals(), returns filtered animal list where (tileHighlighted(animal.levelPos))
+}
 function gameUpdatePost() {}
 function gameRender() {
-	// draw a black square for the background
+	// background
 	drawRect(cameraPos.subtract(cameraOffset), levelSize, hsl(0, 0, 0));
+	drawRect(vec2(2.5, 2.5), vec2(8), green);
+	drawRect(vec2(2.5, 2.5), vec2(5.25), rgb(0, 0, 0));
+	drawRect(vec2(2.5, 2.5), vec2(5), rgb(1, 1, 1));
 
-	// draw the blocks
+	// renders a tile with a border if the mouse is over it
+	renderTile = (drawPos) => {
+		if (mouseOffLevel()) drawRect(drawPos, vec2(1), green);
+		else if (tileHighlighted(pos)) drawRect(drawPos, vec2(0.95), green);
+		else drawRect(drawPos, vec2(1), green);
+	};
+
 	const pos = vec2();
 	for (pos.x = levelSize.x; pos.x--; )
-		for (pos.y = levelSize.y; pos.y--; ) {
-			const data = getTile(pos);
-			if (data === -1) continue;
-			const drawPos = pos.add(vec2(0.5));
-
-			// draw background
-			const color = tileColors[data];
-			drawRect(drawPos, vec2(0.95), color);
-
-			// use darker color for icon
-			const color2 = color.scale(0.8, 1);
-			drawTile(drawPos, vec2(0.5), tile(data, 64), color2);
-		}
-
-	// draw a grey square at top to cover up incomming tiles
-	drawRect(
-		cameraPos.subtract(cameraOffset).add(vec2(0, levelSize.y)),
-		levelSize,
-		backgroundColor,
-	);
+		for (pos.y = levelSize.y; pos.y--; ) renderTile(pos.add(vec2(0.5)));
 }
 function gameRenderPost() {}
 
-// Startup LittleJS Engine
 engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
