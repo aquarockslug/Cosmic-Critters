3263e529363c9cb1ac2fbc0b6318a27cffd8b070
aqua <aqua.rock.slug@gmail.com>
2024-12-08 20:23:04 -0600

    movement

--- game.js
+++ game.js
@@ -5,9 +5,11 @@ const cameraOffset = vec2(0, -0.5);
 const backgroundColor = hsl(0, 0, 0.2);
 const green = rgb(0, 1, 0);
 
+animalPulse = new Timer(2);
+random = new RandomGenerator();
+
 const levelSize = vec2(5, 5);
 const center = vec2(levelSize.x / 2, levelSize.y / 2);
-const animals = [];
 const animalSpecies = [
 	"bee",
 	"camel",
@@ -31,10 +33,11 @@ const animalSpecies = [
 ];
 
 // % level %
-const scan = () => animals.filter((a) => isHighlighted(toLevelPos(a.pos)));
 const isHighlighted = (pos) => level()[pos.x + pos.y * levelSize.x];
 const toLevelPos = (pos) => vec2(Math.floor(pos.x), Math.floor(pos.y));
 const texturePath = (species) => `animals/${species}.svg`;
+const scan = (animals) =>
+	animals.filter((a) => isHighlighted(toLevelPos(a.pos)));
 const level = () => {
 	// a level where none of the tiles are highlighted
 	const emptyLevel = (newLevel = []) => {
@@ -54,19 +57,22 @@ const mouseOffLevel = () =>
 	mousePos.y > levelSize.y;
 
 // % animals %
-const newAnimal = (pos) => {
+const newAnimal = (animals, pos, angle) => {
 	startPos = () =>
 		vec2(
 			Math.random() < 0.5 ? -0.5 : levelSize.x + 0.5,
 			Math.floor(Math.random() * levelSize.y) + 0.5,
 		);
+	start = pos ? pos : startPos();
 	animals.push(
 		new EngineObject(
-			pos ? pos : startPos(),
+			start,
 			vec2(0.2),
-			tile(vec2(0), 1, Math.floor(Math.random() * animalSpecies.length)),
+			tile(vec2(0), 1, random.int(0, animalSpecies.length)),
+			Math.random() * Math.PI * (center.x > start.x ? 1 : -1),
 		),
 	);
+	return [...animals];
 };
 
 // % game %
@@ -74,9 +80,7 @@ function gameInit() {
 	canvasFixedSize = vec2(720, 720);
 	mainCanvas.style.background = backgroundColor;
 
-	newAnimal();
-	newAnimal(vec2(2.5, 2.5));
-	newAnimal(vec2(2.75, 2.75));
+	animals = [];
 
 	cameraPos = levelSize.scale(0.5).add(cameraOffset);
 	cameraScale = 500 / levelSize.y;
@@ -85,9 +89,19 @@ function gameUpdate() {
 	// randomly spawn animal at interval
 
 	// move all animals in animals list
-
+	animals.map((a) => {
+		a.pos = a.pos.lerp(
+			a.pos.add(new Vector2().setAngle(a.angle, 1)),
+			(animalPulse.get() * -1) / 100,
+		);
+		return a;
+	});
+	if (animalPulse.elapsed()) {
+		animals = newAnimal(animals);
+		animalPulse.set(2);
+	}
 	if (mouseWasPressed(0)) {
-		scanned = scan();
+		scanned = scan(animals);
 		if (scanned.length > 0) console.log(scanned);
 		// compare scanned to the set of all scanned species
 	}
